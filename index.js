16498b25b669012029614b6857846acaa55743ba
Brad Bryant <bradleybryant86@gmail.com>
2020-10-20 01:07:09 -0500

    Made process.env.port

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ app.get('/api/courses', (req, res) => {
     res.send([1, 2, 3, 4, 5])
 });
 
-app.listen(3000, () => {
-    console.log("Listening on PORT 3000...")
-});
+// PORT
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on Port ${port}...`));
 
 //To Run Server: nodemon index.js
