915e76ff46064afba4e3dd1ab4262bde5fbb6418
Brad Bryant <bradleybryant86@gmail.com>
2020-10-20 01:03:18 -0500

    Installed nodemon to watch server

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello world!')
+    res.send('What is going on?')
 });
 
 app.get('/api/courses', (req, res) => {
-    res.send([1, 2, 3])
-})
+    res.send([1, 2, 3, 4, 5])
+});
 
 app.listen(3000, () => {
     console.log("Listening on PORT 3000...")
-})
+});
+
+//To Run Server: nodemon index.js
