46606fc27cdc05592cfb588d0619fc2df38b0a53
Brad Bryant <bradleybryant86@gmail.com>
2020-10-21 00:19:12 -0500

    Changed get route for courses/:id to look through courses array or else return 404

--- index.js
+++ index.js
@@ -1,17 +1,40 @@
 const express = require('express');
 const app = express();
 
+const courses = [
+    {
+        id: 1,
+        name: 'course1'
+    },
+    {
+        id: 2,
+        name: 'course2'
+    },
+    {
+        id: 3,
+        name: 'course3'
+    }
+];
+
 app.get('/', (req, res) => {
     res.send('Hello world!')
 });
 
 app.get('/api/courses', (req, res) => {
-    res.send([1, 2, 3, 4, 5])
+    res.send(courses)
 });
 
 app.get('/api/courses/:id', (req, res) => {
-    res.send(req.params.id);
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    //All arrays have the find method, use courses.find
+    //Need to use parseInt otherwise req.params.id would return a string
+    if (!course) res.status(404).send('The course with the given ID was not found');
+    res.send(course);
 });
+
+//req.params object is :year/:month. Essential/required parameters.
+//Query string parameters follow a question mark. These are optional. req.query
+//ex. ?SortBy=name
 
 // PORT
 const port = process.env.PORT || 3000;
